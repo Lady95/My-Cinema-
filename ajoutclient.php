8ee2b950f7e4ff8719575d67b2e45375603917f4
hady <hady.sane@epitech.com>
2018-12-10 17:40:32 +0100

    test

---
+++ ajoutclient.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Formulaire Ajouter un client</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <h1>Formulaire : Ajouter un client</h1>
+        <form action="get">
+            <p>
+                <label for="nom">Nom :</label>
+                <input type="text" name="nom" id="nom">
+                <br>
+                <label for="prenom">Prénom : </label>
+                <input type="text" name="prenom" id="prenom">
+                <br>
+                <label for="datenaissance">Date de naissance : </label>
+                <input type="text" name="datenaissance" id="datenaissance">
+                <br>
+                <label for="email">Adresse email : </label>
+                <input type="text" name="email" id="email">
+                <br>
+                <label for="adresse">Adresse : </label>
+                <input type="text" name="adresse" id="adresse">
+                <br>
+                <label for="cpostal">Code postal : </label>
+                <input type="text" name="cpostal" id="cpostal">
+                <br>
+                <label for="ville">Ville : </label>
+                <input type="text" name="ville" id="ville">
+                <br>
+                <label for="pays">Pays : </label>
+                <input type="text" name="pays" id="pays">
+            </p>
+        </form>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Cinema City</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    </head>
+    <body>
+
+        <h1>Cinema City</h1>
+
+        <h3>Recherche films</h3>
+        <form>
+            <input type="text" name="Recherche">
+            <button>Entrer</button>
+        </form>
+    </body>
+</html>
