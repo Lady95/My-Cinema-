e598db4aeabecebbc1de02e4dd8f0e7cbbde5baa
hady <hady.sane@epitech.com>
2018-12-23 23:32:00 +0100

    maj

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
       <script src="http://code.jquery.com/jquery-3.3.1.js"></script>
    </head>
    <body>
-        
       <div>
          <header>
             <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
@@ -18,7 +17,7 @@
                <img src="img/popcorn.png" alt="logo_popcorn">
                <span class="navbar-brand mb-0 h1">My Cinéma</span>
                </a>
-               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+               <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -33,58 +32,53 @@
                </div>
             </nav>
          </header>
-
          <div class="container" style="margin-top:100px">
             <form method="get" action="recherchefilms.php">
                <h3>Recherche film</h3>
                <div class="form-group">
-                    <input class="form-control" type="search" name='search_film' placeholder="Titre du film" aria-label="Search">
-                </div>
-
-               <div class="input-group mb-3">
-               <div class="input-group-prepend">
-               <label class="input-group-text" for="genre">Genre</label>
+                  <input class="form-control" type="search" name='search_film' placeholder="Titre du film" aria-label="Search">
                </div>
-               <select class="custom-select" name="genre" id="genre">
-                  <option value="" selected>Veuillez selectionner</option>
-                  <?php foreach($genres as $genre): ?>
-                  <option value="<?=$genre['id_genre']?>"><?= $genre['nom']?></option>
-                  <?php endforeach;?>
-               </select>
-               </div>
-
                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <label class="input-group-text" for="distrib">Distributeur</label>
-                    </div>
-                    <select class="custom-select" name="distrib" id="distrib">
-                        <option value="" selected>Veuillez selectionner</option>
-                        <?php foreach($distribs as $distrib): ?>
-                        <option value="<?=$distrib['id_distrib']?>"><?= $distrib['nom']?></option>
-                        <?php endforeach;?>
-                    </select>
+                  <div class="input-group-prepend">
+                     <label class="input-group-text" for="genre">Genre</label>
+                  </div>
+                  <select class="custom-select" name="genre" id="genre">
+                     <option value="" selected>Veuillez selectionner</option>
+                     <?php foreach($genres as $genre): ?>
+                     <option value="<?=$genre['id_genre']?>"><?= $genre['nom']?></option>
+                     <?php endforeach;?>
+                  </select>
+               </div>
+               <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                     <label class="input-group-text" for="distrib">Distributeur</label>
+                  </div>
+                  <select class="custom-select" name="distrib" id="distrib">
+                     <option value="" selected>Veuillez selectionner</option>
+                     <?php foreach($distribs as $distrib): ?>
+                     <option value="<?=$distrib['id_distrib']?>"><?= $distrib['nom']?></option>
+                     <?php endforeach;?>
+                  </select>
                </div>
                <button class="btn btn-primary btn-lg btn-block" type="submit" name="valider">Recherche</button>
             </form>
             <br>
             <div class="container">
-
-            <form method="get" action="verif-search.php">
-               <h3>Recherche membre</h3>
-               <div class="form-group">
-               <input class="form-control" type="search" name="terme" placeholder="Nom et/ou Prénom">
-               </div>
-               <input class="btn btn-secondary btn-lg" type="submit" name="button_search" value="Recherche">
-            </form>
-            <br>
-            <form method="get" action="listes_clientshisto.php">
-               <h3>Historique du membre</h3>
-               <div class="form-group">
-               <input class="form-control" type="search" name="histomembre" placeholder="Nom et/ou Prénom">
-               </div>
-               <input class="btn btn-secondary btn-lg" type="submit" value="Recherche">
-            </form>
-
+               <form method="get" action="verif-search.php">
+                  <h3>Recherche membre</h3>
+                  <div class="form-group">
+                     <input class="form-control" type="search" name="terme" placeholder="Nom et/ou Prénom">
+                  </div>
+                  <input class="btn btn-secondary btn-lg" type="submit" name="button_search" value="Recherche">
+               </form>
+               <br>
+               <form method="get" action="listes_clientshisto.php">
+                  <h3>Historique du membre</h3>
+                  <div class="form-group">
+                     <input class="form-control" type="search" name="histomembre" placeholder="Nom et/ou Prénom">
+                  </div>
+                  <input class="btn btn-secondary btn-lg" type="submit" value="Recherche">
+               </form>
             </div>
          </div>
       </div>
